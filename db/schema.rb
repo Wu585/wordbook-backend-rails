8a3d047f64ef9bf068a4582f1aa347649cbb254a
John Doe <johndoe@example.com>
2022-12-02 10:05:22 +0800

    测试validation code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_091423) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_014154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_091423) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "validation_codes", force: :cascade do |t|
+    t.string "email"
+    t.integer "kind", default: 1, null: false
+    t.string "code", limit: 64
+    t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
