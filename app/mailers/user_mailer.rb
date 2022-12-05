084e9ab77751ab42893bafb8f9f8909c0457afda
John Doe <johndoe@example.com>
2022-12-05 10:45:57 +0800

    重构：让validation_code更简洁

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
     validation_code = ValidationCode.find_by_email email
-    @code = validation_code
+    @code = validation_code.code
     mail(to: email, subject: 'WordBook验证码!')
   end
 end
