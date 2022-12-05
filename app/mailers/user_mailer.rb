5f6441fcd240ff251bfa2d9b4a056f2a70f06d81
John Doe <johndoe@example.com>
2022-12-05 09:09:05 +0800

    测试邮件被成功发送

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(code)
-    @code = code
-    mail(to: '15151851516@163.com', subject: 'Hi!')
+  def welcome_email(email)
+    validation_code = ValidationCode.find_by_email email
+    @code = validation_code
+    mail(to: email, subject: 'WordBook验证码!')
   end
 end
