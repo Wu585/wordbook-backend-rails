9276f953d5b208c186f468c9ce86391aca459cf8
John Doe <johndoe@example.com>
2022-12-06 21:16:50 +0800

    完成 jwt 测试用例

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -4,9 +4,18 @@ class AutoJwt
   end
 
   def call(env)
+    # 不需要 jwt 的接口
+    return @app.call(env) if ['/api/v1/session','/api/v1/validation_codes'].include? env['PATH_INFO']
+    # 需要 jwt 的接口
     header = env['HTTP_AUTHORIZATION']
     token = header.split(' ')[1] rescue ''
-    payload = JWT.decode token, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue nil
+    begin
+      payload = JWT.decode token, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' }
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({ reason: 'token expired' })]]
+    rescue
+      return [401, {}, [JSON.generate({ reason: 'token invalid' })]]
+    end
     env['current_user_id'] = payload[0]['user_id'] rescue nil
     @status, @headers, @response = @app.call(env)
     [@status, @headers, @response]
