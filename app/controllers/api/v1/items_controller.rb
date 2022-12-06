c085fd828804ede51936790eae1ae3600b3d26ba
John Doe <johndoe@example.com>
2022-12-06 14:15:39 +0800

    完成items按时间筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page]).per(params[:per_page])
+    items = Item.where({ created_at: params[:created_after]..params[:created_before] })
+                .page(params[:page]).per(params[:per_page])
     render json: {
       resource: items,
       count: Item.count
