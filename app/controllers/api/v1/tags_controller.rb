5b16933bf6baabd21ab9a6e43fc742a04533d5ee
John Doe <johndoe@example.com>
2022-12-07 23:03:01 +0800

    更新api文档

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -3,6 +3,13 @@ class Api::V1::TagsController < ApplicationController
     current_user_id = request.env["current_user_id"]
     return head :unauthorized if current_user_id.nil?
     tags = Tag.where(user_id: current_user_id).page(params[:page])
-    render json: { resource: tags }
+    render json: {
+      resource: tags,
+      pager: {
+        page: params[:page] || 1,
+        per_page: Item.default_per_page,
+        count: Tag.count
+      }
+    }
   end
 end
