9e3476151e67e7652d57a81bfbbfe49d1cdadf0c
John Doe <johndoe@example.com>
2023-01-09 06:53:17 +0000

    创建 words 表和 word model

---
+++ db/migrate/20230109064714_create_words.rb
@@ -0,0 +1,11 @@
+class CreateWords < ActiveRecord::Migration[7.0]
+  def change
+    create_table :words do |t|
+      t.references :user, null: false, foreign_key: false
+      t.string :content, null: false
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_065559) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_064714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_065559) do
     t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "content", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
 end
